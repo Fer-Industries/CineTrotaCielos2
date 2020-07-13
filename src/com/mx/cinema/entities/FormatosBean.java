c5722a9ba83ccaf7d9cfb0cbec1bf908cc54e6cc
Fabian Isay <fabianisay27@gmail.com>
2020-07-13 00:05:07 -0500

    se agregaron mas controllers

---
+++ src/com/mx/cinema/entities/FormatosBean.java
@@ -0,0 +1,38 @@
+package com.mx.cinema.entities;
+
+public class FormatosBean {
+	
+	private int idFormato;
+	private String nombreFormato;
+	private int precioFormato;
+	
+	public FormatosBean() {
+		
+	}
+
+	public int getIdFormato() {
+		return idFormato;
+	}
+
+	public void setIdFormato(int idFormato) {
+		this.idFormato = idFormato;
+	}
+
+	public String getNombreFormato() {
+		return nombreFormato;
+	}
+
+	public void setNombreFormato(String nombreFormato) {
+		this.nombreFormato = nombreFormato;
+	}
+
+	public int getPrecioFormato() {
+		return precioFormato;
+	}
+
+	public void setPrecioFormato(int precioFormato) {
+		this.precioFormato = precioFormato;
+	}
+	
+   
+}
