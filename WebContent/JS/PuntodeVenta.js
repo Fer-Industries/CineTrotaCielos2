bd8808f6013d31ea26ca5a423e5ee5143e156a5d
Jimmy111962 <jimmy111962@hotmail.com>
2020-12-24 13:35:33 -0600

    Funciona eliminar, se modificaron la mayoria de las funciones relacionadas al carrito, se agregaron comentarios de cada metodo

--- WebContent/JS/PuntodeVenta.js
+++ WebContent/JS/PuntodeVenta.js
@@ -156,102 +156,146 @@ $(function() {
 
 
 /*Funcioneeees del caritooooooooooooooooooooooooooo */
+/*variable global que se va a estar jugando con el carro */
 let carro= [];
 
 const carrito = (producto) =>{
-		let bandera = 0;
-		//console.log("h en arreglar carrito");
-		//console.log(can);
-		bandera = existente(producto)
-		if(bandera == 0){
-			arregloProductos.forEach(x=>{
-				if(producto == x.id){
-					carro.push(x);	
-				}	
-			});
-			carro.forEach(produ =>{
-				if(producto == produ.id){
-					produ.cantidad=document.getElementById("cantidadTabla"+producto).value;
-				}
-			});	
-			grantotal();
-		}
-		return bandera;
+/* 
+	metodo llamado carrito el cual recibe el id del producto que se quiere agregar al carrito y este lo que hace es 
+	del arreglo que se recibe ce la BD compara que el id exista y si existe lo setea al Arreglo global carro 
+	y tambien se le asigna la cantidad que se ordena de ese producto
+ */
+		arregloProductos.forEach(x=>{
+			if(producto == x.id){
+				carro.push(x);	
+			}	
+		});
+		carro.forEach(produ =>{
+			if(producto == produ.id){
+				produ.cantidad=document.getElementById("cantidadTabla"+producto).value;
+			}
+		});	
+		grantotal();
 };
 const existente = (producto) =>{
+/*metodo que sirve para saber si el producto que se quiere agregar ya se encuentra en el arreglo globar carro
+  o si este esta vacio si este esta vacio o si ya se encuentra el producto se regresara una bandera = 1 lo cual indica 
+  que no se debe de hacer nada.
+ */
 	console.log("Empieza existente");
 	let bandera = 0;
-	carro.forEach(produ =>{
+	//console.log(carro.length);
+	//console.log(carro);
+	if(carro.length > 0){
+		carro.forEach(produ =>{
 			if(producto == produ.id){
 				bandera=1;
 			}	
 		});
-	console.log("Termina existente");
-	return bandera
+	console.log("Termina existente");	
+	}
+	return bandera;
 }
 function agregarAlCarrito(elemento){
+/*
+  Esta funcion se manda a llamar en las cards y recibe como parametro el id del producto a agregar al carrito 
+  este lo que hace es invocar a existente lo cual nos dira si se debe de agregar o no 0 significa agregar
+  si se agrega se manda a llamar el metodo carrito para agregarlo al arreglo global y crearCarrito para crear la tabla del nuevo carro
+ */
 		console.log("Estoy en agregar al carrito");
-		carrito(elemento);
-		crearCarrito();
+		let bandera = existente(elemento);
+		if(bandera == 0){
+			carrito(elemento);
+			crearCarrito(carro);
+		}
 		console.log("Termina agregar al carrito");
 }
  
-function crearCarrito(){
+function crearCarrito(nuevocarro){
+	/*
+	Este metodo lo que hace es crear en bodycarrito los productos que se encuentren en el arreglo que recibe como parametro 
+	tambien borra lo que ya existia con anterioridad en bodycarro. 
+	*/
 	let i = 1;
 	let list = document.getElementById("bodycarrito");
-				while(list.hasChildNodes()){
-			    	list.removeChild(list.firstChild);
-			  	}
-				carro.forEach(producto =>{
-					$("#bodycarrito").append("<tr id='"+"carritoid"+producto.id+"'>"
-				      +"<td class='text-center'><button class='btn btn-light' onclick='borrar("+producto.id+")'> <i class='fas fa-trash iconoBasura'></i> </button></td>"
-				      +"<td scope='row'><img src='"+producto.imagen+"'  class='mx-auto rounded d-block' width=50 height=50 alt=Houston tenemos Problemas></td>"
-				      +"<th>"+i+"</th>"
-				      +"<td>"+producto.id+"</td>"
-				      +"<td>"+producto.producto+"</td>"
-				      +"<td>"
-					  +"<input  id='"+"cantidadCarrito"+producto.id+"' onfocusout='actualizarCantidad("+producto.id+")' onchange='out(cantidadCarrito"+producto.id+")' class='cantidadCarrito border mt-1' min=1 max=99  value="+producto.cantidad+">" 				 		     
-				      +"</td>"
-				      +"<td><input id='"+"total"+producto.id+"' value="+producto.precio * producto.cantidad+"></td>"
-				    +"</tr>"
-					);
-				i++;	
-				});
+		while(list.hasChildNodes()){
+	    	list.removeChild(list.firstChild);
+	  	}
+		nuevocarro.forEach(producto =>{
+			$("#bodycarrito").append("<tr id='"+"carritoid"+producto.id+"'>"
+		      +"<td class='text-center'><button class='btn btn-light' onclick='borrar("+producto.id+")'> <i class='fas fa-trash iconoBasura'></i> </button></td>"
+		      +"<td scope='row'><img src='"+producto.imagen+"'  class='mx-auto rounded d-block' width=50 height=50 alt=Houston tenemos Problemas></td>"
+		      +"<th>"+i+"</th>"
+		      +"<td>"+producto.id+"</td>"
+		      +"<td>"+producto.producto+"</td>"
+		      +"<td>"
+			  +"<input  id='"+"cantidadCarrito"+producto.id+"' onfocusout='actualizarCantidad("+producto.id+")' onchange='out(cantidadCarrito"+producto.id+")' class='cantidadCarrito border mt-1' min=1 max=99  value="+producto.cantidad+">" 				 		     
+		      +"</td>"
+		      +"<td><input id='"+"total"+producto.id+"' value="+producto.precio * producto.cantidad+"></td>"
+		    +"</tr>"
+		);
+		i++;	
+	});
 }
 
 function grantotal(){
+	/* 
+	  Este metodo lo que hace es del arreglo global carro 
+	  voy obtener el total de cada producto que se encuentra en el arreglo carro 
+	  y se van a sumar a la variable supertotal que es lo que hace que tenga la suma general
+	  y  al valor de supertotal se le asigna como valor al elemento html grantotal
+	*/
 	let supertotal = 0;
 	console.log("estoy en gran total");
-	console.log(carro);
 	carro.forEach(total=>{
 		supertotal= supertotal + total.precio*total.cantidad;
 	})
-	console.log(supertotal);
 	document.getElementById("grantotal").value = supertotal;
 	console.log("termina gran total");
 };
 
 function actualizarCantidad(id){
+	/*
+	Se manda a llamar en el evento onfocusout del elemento html  input relacionado al id del producto
+	esto va a obtener el valor que se encuentre en el input cantidadCarritoid id se sustituye por el id del producto
+	donde los ids coincidan en el arreglo carro se va a cambiar la cantidad del carro a la nueva cantidad que es total
+	y luego en en el input totalid id se sustituye se va a setar el nuevo valor. 
+	 */
 	console.log("Estoy en actualizar Cantidad");
 	let total = document.getElementById("cantidadCarrito"+id).value;
 	carro.forEach(produ =>{
-		console.log("estoy comparando "+produ.id+"con "+id);
 		if(id == produ.id){
 			produ.cantidad=total;
-			console.log("la nueva cantidad es");
-			console.log(produ.total);
-			console.log("voy a multiplicar"+ total +" por "+produ.precio);
-			console.log(total * produ.precio);
 			document.getElementById("total"+id).value = produ.cantidad * produ.precio;
 		}		
 	 });
-grantotal();
+	grantotal();
+	console.log("Termina actualizar cantidad");
 }
 
 function borrar(id){
+	/* borra el elemento del html bodycarrito y tambien del arreglo global carro
+	  pero se hace trampa ya que no se borra en realidad se hace un nuevo arreglo llamado n 
+	 */
+	console.log("comienza borrar");
+	let n = carro.filter(producto=>{
+		if(producto.id != id){
+			return producto;
+		}
+	}) ;
+	carro = n;
+	crearCarrito(n);
+	console.log("Termina borrar");
+}
+
+
+/*Funcioneeees del Terminaa caritooooooooooooooooooooooooooo */
+
+/* Para hacer pruebas 
+function borrar(id){
 	console.log("comienza borrar");
 	console.log(carro);
-	let i=0;
+/*	let i=0;
 	let bandera= 0;
 	carro.forEach(product=>{
 		if(product.id = id){
@@ -261,7 +305,7 @@ function borrar(id){
 	});
 	console.log("bandera vale:");
 	console.log(bandera);
-	carro.splice(bandera,1); 
+	carro.splice(bandera,1); */ 
 	
 /*	let ids= [];
 	carro.forEach(product=>{
@@ -275,18 +319,36 @@ function borrar(id){
 	console.log(indexaBorrar);
 	if (indexaBorrar > -1) {
   		carro.splice(indexaBorrar, 1);
-	} */	 
-	 
+	}  
+	let n = carro.filter(producto=>{
+		if(producto.id != id){
+			return producto;
+		}
+	}) ;
 	console.log("carro vale");
 	console.log(carro);
-	crearCarrito()
-	console.log("Termina borrar");
+	console.log(n);
 	
+	crearCarrito(n);
+	console.log("Termina borrar");
+	return n;
+}
+  
+function actualizarCantidad(id){
+	//console.log("Estoy en actualizar Cantidad");
+	let total = document.getElementById("cantidadCarrito"+id).value;
+	carro.forEach(produ =>{
+		//console.log("estoy comparando "+produ.id+"con "+id);
+		if(id == produ.id){
+			produ.cantidad=total;
+			//console.log("la nueva cantidad es");
+			//console.log(produ.total);
+			//console.log("voy a multiplicar"+ total +" por "+produ.precio);
+			//console.log(total * produ.precio);
+			document.getElementById("total"+id).value = produ.cantidad * produ.precio;
+		}		
+	 });
+	grantotal();
 }
 
-
-/*Funcioneeees del caritooooooooooooooooooooooooooo */
-
-/*
- ir te de viaje cancun  
  */
